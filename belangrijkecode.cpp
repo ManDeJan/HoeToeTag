0837113e3e1c9fea778a7c9ac25002ae1966ab15
ManDeJan <janhlsm@gmail.com>
2017-04-26 14:12:53 +0200

    Added important code

---
+++ belangrijkecode.cpp
@@ -0,0 +1,13 @@
+
+int main(int argc, char const *argv[]) {
+
+    int x = 8;
+
+    while (x) {
+        //Super belangrijke code
+        std::cout << "Bliep bloop" << '\n';
+        x--;
+    }
+
+    return 0;
+}
